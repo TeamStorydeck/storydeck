e6f8e09642ec960ad6a5229cae2075c80111abc9
Asim Bera <asimbera@outlook.in>
2021-03-19 21:01:01 +0530

    create search page

---
+++ lib/views/home/search.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:storydeck/views/home/common.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leading: IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => null,
+          ),
+          title: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Search",
+            ),
+            style: Theme.of(context).primaryTextTheme.bodyText1,
+          ),
+          centerTitle: true,
+          actions: [TopBarActionButton()],
+        ),
+        SliverFillRemaining(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.sticky_note_2_rounded,
+                  size: 65,
+                ),
+                SizedBox(height: 5),
+                Text("Your search history will appear here"),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
